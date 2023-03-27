80ff67143351e9dbca35b425018a4554d1774186
Maxim Fateev <mfateev@gmail.com>
2023-03-27 10:07:36 +0100

    Fixed variable name in unit test

--- src/test/java/io/temporal/samples/moneytransfer/TransferWorkflowTest.java
+++ src/test/java/io/temporal/samples/moneytransfer/TransferWorkflowTest.java
@@ -50,11 +50,15 @@ public class TransferWorkflowTest {
         testWorkflowRule
             .getWorkflowClient()
             .newWorkflowStub(AccountTransferWorkflow.class, options);
-    long starty = testWorkflowRule.getTestEnvironment().currentTimeMillis();
+
+    long start = testWorkflowRule.getTestEnvironment().currentTimeMillis();
+
     workflow.transfer("account1", "account2", "reference1", 123);
+
     verify(activities).withdraw(eq("account1"), eq("reference1"), eq(123));
     verify(activities).deposit(eq("account2"), eq("reference1"), eq(123));
-    long duration = testWorkflowRule.getTestEnvironment().currentTimeMillis() - starty;
+
+    long duration = testWorkflowRule.getTestEnvironment().currentTimeMillis() - start;
     System.out.println("Duration: " + duration);
 
     testWorkflowRule.getTestEnvironment().shutdown();
