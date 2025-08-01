c66c20a78de9c595f06d54272e57fadb71ca7393
Maxim Fateev <mfateev@gmail.com>
2025-08-01 16:21:06 -0700

    Fixed compensation cleanup

--- core/src/main/java/io/temporal/samples/retryonsignalinterceptor/ActivityCompensation.java
+++ core/src/main/java/io/temporal/samples/retryonsignalinterceptor/ActivityCompensation.java
@@ -6,7 +6,7 @@ public class ActivityCompensation {
   public static AutoCloseable newCompensation(Functions.Proc1<Exception> compensation) {
     RetryOnSignalWorkflowOutboundCallsInterceptor.setCompensation(compensation);
     return () -> {
-      RetryOnSignalWorkflowOutboundCallsInterceptor.setCompensation(null);
+      RetryOnSignalWorkflowOutboundCallsInterceptor.cleanCompensation();
     };
   }
 }
